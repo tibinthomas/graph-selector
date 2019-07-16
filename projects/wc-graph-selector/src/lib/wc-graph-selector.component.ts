f30328729cfdc1d31b1df071f5dab7ca3f43b9d3
Tibin Thomas <tibinthomas@outlook.com>
2019-07-17 00:49:41 +0530

    x y axis and axis coridinates

--- projects/wc-graph-selector/src/lib/wc-graph-selector.component.ts
+++ projects/wc-graph-selector/src/lib/wc-graph-selector.component.ts
@@ -1,19 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  AfterViewInit
+} from "@angular/core";
 
 @Component({
-  selector: 'lib-wc-graph-selector',
-  template: `
-    <p>
-      wc-graph-selector works!
-    </p>
-  `,
-  styles: []
+  selector: "lib-wc-graph-selector",
+  templateUrl: "./wc-graph-selector.component.html",
+  styleUrls: ["./wc-graph-selector.component.scss"]
 })
-export class WcGraphSelectorComponent implements OnInit {
+export class WcGraphSelectorComponent implements OnInit, AfterViewInit {
+  @Input() width;
+  @Input() height;
+  @ViewChild("canvas", { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+  private ctx: CanvasRenderingContext2D;
 
-  constructor() { }
+  y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+  spaceBetweenYaxisSegemnts;
+  spaceBetweenXaxisSegemnts;
+
+  constructor() {}
 
   ngOnInit() {
+    this.ctx = this.canvas.nativeElement.getContext("2d");
   }
 
+  ngAfterViewInit() {
+    this.spaceBetweenYaxisSegemnts = this.height / this.y.length;
+    this.spaceBetweenXaxisSegemnts = this.width / this.x.length;
+    /**
+     * X axis and Y axis
+     */
+    this.ctx.moveTo(this.width / 2, 0);
+    this.ctx.lineTo(this.width / 2, this.height);
+    this.ctx.moveTo(0, this.height / 2);
+    this.ctx.lineTo(this.width, this.height / 2);
+
+    console.log(this.spaceBetweenXaxisSegemnts);
+
+    /**
+     * X axis segments
+     */
+    for (let i = 1; i <= this.width / this.spaceBetweenXaxisSegemnts; i++) {
+      this.ctx.moveTo(this.spaceBetweenXaxisSegemnts * i, this.height / 2 - 7);
+      this.ctx.lineTo(this.spaceBetweenXaxisSegemnts * i, this.height / 2 + 7);
+    }
+
+    /**
+     * Y axis segments
+     */
+    for (let i = 1; i <= this.height / this.spaceBetweenYaxisSegemnts; i++) {
+      this.ctx.moveTo(this.width / 2 - 7, this.spaceBetweenXaxisSegemnts * i);
+      this.ctx.lineTo(this.width / 2 + 7, this.spaceBetweenXaxisSegemnts * i);
+    }
+
+    this.ctx.stroke();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WcGraphSelectorModule } from 'projects/wc-graph-selector/src/public-api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WcGraphSelectorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
